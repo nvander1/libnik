f2bcfa8043ec8eb7d61bfff3d850294d9b1844bf
Nikolas Vanderhoof <nikolasrvanderhoof@gmail.com>
2018-02-14 15:16:07 -0500

    Put opening braces on same line

--- include/main.hpp
+++ include/main.hpp
@@ -7,8 +7,7 @@
 int nik_main(std::vector<std::string> args);
 
 #define main()                                                                 \
-  main(int argc, char **argv)                                                  \
-  {                                                                            \
+  main(int argc, char **argv) {                                                \
     std::vector<std::string> args{};                                           \
     for (int i = 0; i < argc; ++i) {                                           \
       args.push_back(std::string{argv[i]});                                    \
