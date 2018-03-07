56a9076e2130f32875fe6302936e605df45c59db
Nikolas Vanderhoof <nikolasrvanderhoof@gmail.com>
2018-03-07 00:15:09 -0500

    Convert nik::string into a class template

--- include/string.hpp
+++ include/string.hpp
@@ -12,12 +12,18 @@
 
 namespace nik {
 
-class string : public std::string {
+template <typename CharT, typename Traits = std::char_traits<CharT>,
+          typename Allocator = std::allocator<CharT>>
+class basic_string : public std::basic_string<CharT, Traits, Allocator> {
+  using stl_basic_string = std::basic_string<CharT, Traits, Allocator>;
 
 public:
-  using std::string::string;
-  string(std::string other) : std::string{other} {}
-  string capitalize() {
+  // Member functions
+  using stl_basic_string::basic_string;
+
+  basic_string(stl_basic_string other) : stl_basic_string{other} {}
+
+  basic_string capitalize() {
     auto copied = *this;
     auto &front_char = copied.front();
     front_char = std::toupper(front_char);
@@ -25,69 +31,69 @@ public:
   }
 
   // https://github.com/python/cpython/blob/master/Objects/stringlib/transmogrify.h#L156-L174
-  string center(int width, char fillchar = ' ') {
-    if (size() >= width)
+  basic_string center(int width, char fillchar = ' ') {
+    if (this->size() >= width)
       return *this;
 
-    auto marg = width - size();
+    auto marg = width - this->size();
     auto left = marg / 2 + (marg & width & 1);
 
     return pad(left, marg - left, fillchar);
   }
 
-  size_t count(std::string sub, size_t start = 0, size_t end = 0) {
+  size_t count(stl_basic_string sub, size_t start = 0, size_t end = 0) {
     if (end == 0)
-      end += size();
-    auto searched_portion = substr(start, end - start);
+      end += this->size();
+    auto searched_portion = this->substr(start, end - start);
     size_t seen = 0;
     int pos = -1;
     while ((pos = searched_portion.find(sub, static_cast<size_t>(pos + 1))) !=
-           string::npos) {
+           basic_string::npos) {
       seen++;
     }
     return seen;
   }
 
-  std::vector<nik::string> split(std::string sep) {
-    auto tokens = std::vector<nik::string>();
+  std::vector<stl_basic_string> split(stl_basic_string sep) {
+    auto tokens = std::vector<stl_basic_string>();
     size_t prev = 0;
     size_t next = 0;
 
-    while ((next = find(sep, prev)) != string::npos) {
-      tokens.push_back(substr(prev, next - prev));
+    while ((next = this->find(sep, prev)) != basic_string::npos) {
+      tokens.push_back(this->substr(prev, next - prev));
       prev = next + sep.size();
     }
-    tokens.push_back(substr(prev, next - prev));
+    tokens.push_back(this->substr(prev, next - prev));
 
     return tokens;
   }
 
-  std::vector<nik::string> split(std::string sep, size_t maxsplit) {
-    auto tokens = std::vector<nik::string>();
+  std::vector<stl_basic_string> split(stl_basic_string sep, size_t maxsplit) {
+    auto tokens = std::vector<stl_basic_string>();
     if (maxsplit == 0)
       return tokens;
 
     size_t prev = 0;
     size_t next = 0;
 
-    while ((next = find(sep, prev)) != string::npos) {
-      tokens.push_back(substr(prev, next - prev));
+    while ((next = this->find(sep, prev)) != basic_string::npos) {
+      tokens.push_back(this->substr(prev, next - prev));
       if (tokens.size() == maxsplit + 1)
         return tokens;
       prev = next + sep.size();
     }
-    tokens.push_back(substr(prev, next - prev));
+    tokens.push_back(this->substr(prev, next - prev));
 
     return tokens;
   }
 
-  std::vector<nik::string> split(size_t maxsplit = SIZE_MAX) {
-    auto tokens = std::vector<nik::string>();
+  std::vector<stl_basic_string> split(size_t maxsplit = SIZE_MAX) {
+    auto tokens = std::vector<stl_basic_string>();
     if (maxsplit == 0)
       return tokens;
 
     auto iss = std::istringstream(*this);
-    using Iterator = std::istream_iterator<nik::string>;
+    using Iterator = std::istream_iterator<stl_basic_string>;
 
     if (maxsplit == SIZE_MAX)
       std::copy(Iterator(iss), Iterator(), std::back_inserter(tokens));
@@ -99,7 +105,7 @@ public:
 
 private:
   // https://github.com/python/cpython/blob/master/Objects/stringlib/transmogrify.h#L93-L120
-  string pad(size_t left, size_t right, char fill) {
+  basic_string pad(size_t left, size_t right, char fill) {
     if (left < 0)
       left = 0;
     if (right < 0)
@@ -108,12 +114,14 @@ private:
     if (left == 0 && right == 0)
       return *this;
 
-    auto padded = nik::string(left + size() + right, fill);
-    padded.replace(left, size(), *this);
+    auto padded = basic_string(left + this->size() + right, fill);
+    padded.replace(left, this->size(), *this);
 
     return padded;
   }
 };
+
+using string = basic_string<char>;
 
 } // namespace nik
 
