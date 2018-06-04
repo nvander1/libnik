62805dc5f95d0500f5a82598179fe5f8f3427d34
Nikolas Vanderhoof <nikolasrvanderhoof@gmail.com>
2018-06-03 20:33:54 -0400

    Add is_specialization_of

--- include/type_traits.hpp
+++ include/type_traits.hpp
@@ -2,6 +2,11 @@
 
 #include <type_traits>
 
+#ifndef NDEBUG
+#include <tuple>
+#include <vector>
+#endif
+
 namespace nik {
 
 template <typename T>
@@ -11,5 +16,20 @@ struct remove_cvref {
 
 template <typename T>
 using remove_cvref_t = typename remove_cvref<T>::type;
+
+template <typename T, template <typename...> class Template>
+struct is_specialization_of : std::false_type{};
+
+template <template <typename...> class Template, typename... Args>
+struct is_specialization_of<Template<Args...>, Template> : std::true_type{};
+
+template <typename T, template <typename...> class Template>
+constexpr bool is_specialization_of_v = is_specialization_of<T, Template>::value;
+
+#ifndef NDEBUG
+static_assert(is_specialization_of_v<std::tuple<int, double, bool>, std::tuple>);
+static_assert(is_specialization_of_v<std::vector<int>, std::vector>);
+static_assert(!is_specialization_of_v<std::tuple<int>, std::vector>);
+#endif
 
 } // namespace nik
