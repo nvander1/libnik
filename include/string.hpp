da932569c244859027f45983d68690e726d17be3
Nikolas Vanderhoof <nikolasrvanderhoof@gmail.com>
2018-09-17 21:52:54 -0400

    Add basic functionality and test for string adapter

--- include/string.hpp
+++ include/string.hpp
@@ -1,119 +1,33 @@
 #ifndef NIK_STRING_HPP
 #define NIK_STRING_HPP
 
-#include <algorithm>
-#include <cctype>
-#include <cstdint>
-#include <iostream>
-#include <iterator>
-#include <sstream>
 #include <string>
-#include <vector>
 
 namespace nik {
 
-template <typename StringT>
-StringT capitalize(StringT string) {
-  auto& front_char = string.front();
-  front_char = std::toupper(front_char);
-  return string;
-}
+template <typename CharT, typename Traits = std::char_traits<CharT>,
+          typename Allocator = std::allocator<CharT>>
+class basic_string {
+ public:
+  // Member types
+  using StringT = std::basic_string<CharT, Traits, Allocator>;
 
-// https://github.com/python/cpython/blob/master/Objects/stringlib/transmogrify.h#L93-L120
-template <typename StringT>
-StringT pad(StringT string, typename StringT::size_type left,
-            typename StringT::size_type right,
-            typename StringT::value_type fill) {
-  if (left < 0) left = 0;
-  if (right < 0) right = 0;
+  // Member functions
 
-  if (left == 0 && right == 0) return string;
+  /// Constructors
 
-  auto padded = StringT(left + string.size() + right, fill);
-  padded.replace(left, string.size(), string);
+  //// Copy conversion constructor
+  basic_string(const StringT& other) : s{other} {}
 
-  return padded;
-}
+  //// Move conversion constructor
+  basic_string(StringT&& other) : s{other} {}
 
-// https://github.com/python/cpython/blob/master/Objects/stringlib/transmogrify.h#L156-L174
-template <typename StringT>
-StringT center(StringT string, typename StringT::size_type width,
-               typename StringT::value_type fillchar = ' ') {
-  if (string.size() >= width) return string;
+  /// Conversion operator
+  operator StringT() { return s; }
 
-  auto marg = width - string.size();
-  auto left = marg / 2 + (marg & width & 1);
-
-  return pad(string, left, marg - left, fillchar);
-}
-
-template <typename StringT>
-typename StringT::size_type count(StringT string, StringT sub,
-                                  typename StringT::size_type start = 0,
-                                  typename StringT::size_type end = 0) {
-  if (end == 0) end += string.size();
-  auto searched_portion = string.substr(start, end - start);
-  typename StringT::size_type seen = 0;
-  int pos = -1;
-  while ((pos = searched_portion.find(
-              sub, static_cast<typename StringT::size_type>(pos + 1))) !=
-         StringT::npos) {
-    seen++;
-  }
-  return seen;
-}
-
-template <typename StringT>
-std::vector<StringT> split(StringT string, StringT sep) {
-  auto tokens = std::vector<StringT>();
-  typename StringT::size_type prev = 0;
-  typename StringT::size_type next = 0;
-
-  while ((next = string.find(sep, prev)) != StringT::npos) {
-    tokens.push_back(string.substr(prev, next - prev));
-    prev = next + sep.size();
-  }
-  tokens.push_back(string.substr(prev, next - prev));
-
-  return tokens;
-}
-
-template <typename StringT>
-std::vector<StringT> split(StringT string, StringT sep,
-                           typename StringT::size_type maxsplit) {
-  auto tokens = std::vector<StringT>();
-  if (maxsplit == 0) return tokens;
-
-  typename StringT::size_type prev = 0;
-  typename StringT::size_type next = 0;
-
-  while ((next = string.find(sep, prev)) != StringT::npos) {
-    tokens.push_back(string.substr(prev, next - prev));
-    if (tokens.size() == maxsplit + 1) return tokens;
-    prev = next + sep.size();
-  }
-  tokens.push_back(string.substr(prev, next - prev));
-
-  return tokens;
-}
-
-template <typename StringT>
-std::vector<StringT> split(StringT string,
-                           typename StringT::size_type maxsplit = SIZE_MAX) {
-  auto tokens = std::vector<StringT>();
-  if (maxsplit == 0) return tokens;
-
-  auto iss = std::istringstream(string);
-  using Iterator = std::istream_iterator<StringT>;
-
-  if (maxsplit == SIZE_MAX)
-    std::copy(Iterator(iss), Iterator(), std::back_inserter(tokens));
-  else
-    std::copy_n(Iterator(iss), maxsplit, std::back_inserter(tokens));
-
-  return tokens;
-}
-
+  // Members
+  StringT s;
+};
 
 } // namespace nik
 
--- test/string_tests.cpp
+++ test/string_tests.cpp
@@ -1,92 +1,30 @@
 #include "string.hpp"
 
 #include <gtest/gtest.h>
+#include <string>
+#include <utility>
 
-#include <vector>
+TEST(string, copy_conversion_constructor) {
+  auto a = std::string{"hello"};
+  auto b = nik::basic_string{a};
 
-TEST(string, capitalize) {
-  auto s = std::string{"hello"};
-
-  ASSERT_TRUE(nik::capitalize(s) == std::string{"Hello"});
+  ASSERT_TRUE(b.s == a);
 }
 
-TEST(string, center) {
-  std::vector<std::string> inputs{"a", "ab", "abc", "abcd", "abcde"};
+TEST(string, move_conversion_constructor) {
+  auto a = std::string{"hello"};
+  auto b = nik::basic_string{std::move(a)};
 
-  std::vector<std::vector<std::string>> outputs{
-      {"a", "a", "a ", " a ", " a  ", "  a  ", "  a   ", "   a   ", "   a    ",
-       "    a    "},
-      {"ab", "ab", "ab", " ab", " ab ", "  ab ", "  ab  ", "   ab  ",
-       "   ab   ", "    ab   "},
-      {"abc", "abc", "abc", "abc", "abc ", " abc ", " abc  ", "  abc  ",
-       "  abc   ", "   abc   "},
-      {"abcd", "abcd", "abcd", "abcd", "abcd", " abcd", " abcd ", "  abcd ",
-       "  abcd  ", "   abcd  "},
-      {"abcde", "abcde", "abcde", "abcde", "abcde", "abcde", "abcde ",
-       " abcde ", " abcde  ", "  abcde  "}};
-
-  for (auto i = 0; i < inputs.size(); ++i) {
-    for (size_t width = 0; width < 10; ++width) {
-      ASSERT_TRUE(nik::center(inputs[i], width) == outputs[i][width]);
-    }
-  }
-
-  ASSERT_TRUE(nik::center(std::string{"a"}, 5, '_') == std::string{"__a__"});
+  ASSERT_TRUE(b.s == std::string{"hello"});
 }
 
-TEST(string, count) {
-  ASSERT_EQ(5, nik::count(std::string{"aaaaa"}, std::string{"a"}));
-  ASSERT_EQ(2, nik::count(std::string{"hello hello"}, std::string{"hello"}));
-  ASSERT_EQ(1, nik::count(std::string{"hello hello"}, std::string{"hello"}, 1));
-  ASSERT_EQ(1,
-            nik::count(std::string{"hello hello"}, std::string{"hello"}, 0, 6));
-}
+TEST(string, conversion_operator) {
+  auto a = nik::basic_string{std::string{"hello"}};
+  auto b = std::string{a};
 
-TEST(string, split) {
-  auto string = std::string("a\nb\tc  d\t e  ");
+  auto foo = [&](const std::string& x) {
+    return x;
+  };
 
-  auto tokens = nik::split(string);
-  EXPECT_EQ(5, tokens.size());
-  EXPECT_TRUE(tokens[0] == std::string("a"));
-  EXPECT_TRUE(tokens[1] == std::string("b"));
-  EXPECT_TRUE(tokens[2] == std::string("c"));
-  EXPECT_TRUE(tokens[3] == std::string("d"));
-  EXPECT_TRUE(tokens[4] == std::string("e"));
-
-  auto tokens2 = nik::split(string, 2);
-  EXPECT_EQ(2, tokens2.size());
-  EXPECT_TRUE(tokens[0] == std::string("a"));
-  EXPECT_TRUE(tokens[1] == std::string("b"));
-
-  auto tokens_empty = nik::split(string, 0);
-  EXPECT_EQ(0, tokens_empty.size());
-
-  auto csv = std::string("a,b,c,d");
-  auto tokens_sep = nik::split(csv, std::string(","));
-  EXPECT_EQ(4, tokens_sep.size());
-  EXPECT_TRUE(tokens_sep[0] == std::string("a"));
-  EXPECT_TRUE(tokens_sep[1] == std::string("b"));
-  EXPECT_TRUE(tokens_sep[2] == std::string("c"));
-  EXPECT_TRUE(tokens_sep[3] == std::string("d"));
-
-  auto tokens_sep_limited = nik::split(csv, std::string(","), 2);
-  EXPECT_EQ(3, tokens_sep_limited.size());
-  EXPECT_TRUE(tokens_sep_limited[0] == std::string("a"));
-  EXPECT_TRUE(tokens_sep_limited[1] == std::string("b"));
-  EXPECT_TRUE(tokens_sep_limited[2] == std::string("c"));
-  std::cout << tokens_sep_limited[2] << std::endl;
-
-  auto double_sep = std::string("a,,b,,c,,d");
-  auto tokens_double_sep = nik::split(double_sep, std::string(",,"));
-  EXPECT_EQ(4, tokens_double_sep.size());
-  EXPECT_TRUE(tokens_double_sep[0] == std::string("a"));
-  EXPECT_TRUE(tokens_double_sep[1] == std::string("b"));
-  EXPECT_TRUE(tokens_double_sep[2] == std::string("c"));
-  EXPECT_TRUE(tokens_double_sep[3] == std::string("d"));
-
-  auto tokens_double_sep_limited = nik::split(double_sep, std::string(",,"), 2);
-  EXPECT_EQ(3, tokens_double_sep_limited.size());
-  EXPECT_TRUE(tokens_double_sep_limited[0] == std::string("a"));
-  EXPECT_TRUE(tokens_double_sep_limited[1] == std::string("b"));
-  EXPECT_TRUE(tokens_double_sep_limited[2] == std::string("c"));
+  ASSERT_TRUE(b == foo(b));
 }
