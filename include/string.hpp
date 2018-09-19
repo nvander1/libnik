80d119c9cda122cdc0f9f617457a1f73907139a3
Nikolas Vanderhoof <nikolasrvanderhoof@gmail.com>
2018-09-18 22:19:54 -0400

    Add member types similar to those of std::basic_string

--- include/string.hpp
+++ include/string.hpp
@@ -11,6 +11,19 @@ class basic_string {
  public:
   // Member types
   using string_type = std::basic_string<CharT, Traits, Allocator>;
+  using traits_type = typename string_type::traits_type;
+  using value_type = typename string_type::value_type;
+  using allocator_type = typename string_type::allocator_type;
+  using size_type = typename string_type::size_type;
+  using difference_type = typename string_type::difference_type;
+  using reference = typename string_type::reference;
+  using const_reference = typename string_type::const_reference;
+  using pointer = typename string_type::pointer;
+  using const_pointer = typename string_type::const_pointer;
+  using iterator = typename string_type::iterator;
+  using const_iterator = typename string_type::const_iterator;
+  using reverse_iterator = typename string_type::reverse_iterator;
+  using const_reverse_iterator = typename string_type::const_reverse_iterator;
 
   // Member functions
 
